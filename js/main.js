da532659553e68bc7ba066d67bbca24340678e1a
Joshua Miccael Canilang <169007444+chibi-ennix@users.noreply.github.com>
2024-10-02 21:05:51 -0600

    inclass LAB 3
    Event listener

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 // Enter your code below.
+// Step 1: Select the elements from the DOM
+const showResourcesButton = document.getElementById("show-resources");
+const resourcesDiv = document.querySelector(".javascript-resources");
+
+// Step 2: Add an event listener for the click event on the button
+showResourcesButton.addEventListener("click", function() {
+  // Step 3: Remove the "d-none" class to show the resources
+  resourcesDiv.classList.remove("d-none");
+});
